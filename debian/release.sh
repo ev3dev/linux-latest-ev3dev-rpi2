78bae4742c30ec8e678bcd2ddbc2b90c44aae71a
David Lechner <david@lechnology.com>
2019-11-24 17:44:24 -0600

    debian/release: use distro for codename
    server has been updated to only accept codename, not distribution

--- debian/release.sh
+++ debian/release.sh
@@ -7,10 +7,9 @@ set -e
 source=$(dpkg-parsechangelog -S Source)
 version=$(dpkg-parsechangelog -S Version)
 distribution=$(dpkg-parsechangelog -S Distribution)
-codename=$(debian-distro-info --codename --${distribution})
 
-OS=debian DIST=${codename} ARCH=armhf pbuilder-ev3dev build
-debsign ~/pbuilder-ev3dev/debian/${codename}-armhf/${source}_${version}_armhf.changes
-dput ev3dev-debian ~/pbuilder-ev3dev/debian/${codename}-armhf/${source}_${version}_armhf.changes
+OS=debian DIST=${distribution} ARCH=armhf pbuilder-ev3dev build
+debsign ~/pbuilder-ev3dev/debian/${distribution}-armhf/${source}_${version}_armhf.changes
+dput ev3dev-debian ~/pbuilder-ev3dev/debian/${distribution}-armhf/${source}_${version}_armhf.changes
 
 gbp buildpackage --git-tag-only
